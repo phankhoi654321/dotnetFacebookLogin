828ec34688bfdccde1ce51d63f6f93a7ad30a5b7
maikhanhdn6543210 <phankhoi654321@gmail.com>
2018-10-12 22:44:35 +0700

    before Rental function

--- WebWithAuthentication/Controllers/HomeController.cs
+++ WebWithAuthentication/Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace WebWithAuthentication.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        //[OutputCache(Duration = 50, Location = OutputCacheLocation.Server, VaryByParam = "genre")]
         public ActionResult Index()
         {
             return View();
